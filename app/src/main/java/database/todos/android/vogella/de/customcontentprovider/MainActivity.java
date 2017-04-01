d699426cdbdacd16e53a3420b4fa6ef6caffe57b
Seshine <seshinsebastian@gmail.com>
2017-04-01 14:37:58 +0530

    Share app data using content provider

---
+++ app/src/main/java/database/todos/android/vogella/de/customcontentprovider/MainActivity.java
@@ -0,0 +1,15 @@
+package database.todos.android.vogella.de.customcontentprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.vogella.android.todos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
